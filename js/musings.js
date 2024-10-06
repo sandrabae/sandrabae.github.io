ac20ac02da0830409786579ed03ae364c7c1e956
sandrabae <suyun.bae@colorado.edu>
2024-10-06 14:07:26 -0600

    overleaf 101

--- js/musings.js
+++ js/musings.js
@@ -7,8 +7,10 @@ class Musings extends HTMLElement {
     <h2>MUSINGS</h2>
     <p>I often get asked questions about my PhD experience at the ATLAS Institute in addition to other random questions. Following <a href="https://matt.might.net/articles/how-to-blog-as-an-academic/">Matt Might's advice</a>, I started to compile these thoughts into a blog so others can benefit.</p><br>
 
+      <a class="blog-titles" href="https://share.note.sx/ptfdbb5e#q8SaZtFMLTb7p8TNkf7CXIMFg4RFukG5bBhy+F8duCo">Overleaf 101</a> — October 06, 2024<br>
 
  <a class="blog-titles" href="https://share.note.sx/j1259q9t#WsxwrAotEZm2Y2Fcc9zuRDEbMvC3ZCjFUV9DawyJDOo">A Reflection—There and Back</a> — August 20, 2024
+
   `;
     }
 }
